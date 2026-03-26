6eb9f0090c75996fb245ee61e063bd9bc2073c97
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-03-26 22:37:57 +0200

    fix: remove lesson data type
    Signed-off-by: Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>

--- data/lesson.ts
+++ data/lesson.ts
@@ -1,12 +1,11 @@
 import type { DayOfWeek } from "./day-of-week";
 import type { Group } from "./group";
-import type { Subject } from "./subject";
 import type { Teacher } from "./teacher";
 
 export type Lesson = {
   id: number;
   teacher: Teacher;
-  subject: Subject;
+  subject: string;
   group: Group;
   number: number;
   dayOfWeek: DayOfWeek;
--- data/subject.ts
+++
@@ -1,4 +0,0 @@
-export type Subject = {
-  id: number;
-  name: string;
-};
